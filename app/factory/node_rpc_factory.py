87d33e901a2da85b0f2c4862e7e6ba870d3a7766
krails0105 <krails.kim@gmail.com>
2025-04-29 23:54:55 +0900

    test

--- app/factory/node_rpc_factory.py
+++ app/factory/node_rpc_factory.py
@@ -11,4 +11,4 @@ class BitcoinNodeRpcFactory:
         _param_form = deepcopy(cls.param_form)
         _param_form["method"] = "getblock"
         _param_form["params"] = [block_hash, 3]
-        return json.dumps(_param_form)
+        return json.dumps(_param_form) 
